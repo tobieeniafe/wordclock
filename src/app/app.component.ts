2ce4f6bf00ef57d0314e0a45b0a117e42e5afc6a
Samuel <tobisamuel88@gmail.com>
2017-09-05 13:53:21 +0100

    angular2 implementation done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,50 +5,56 @@ import * as moment from 'moment';
 @Component({
   selector: 'app-root',
   styleUrls: ['./app.component.css'],
-  template: `
+  template: `<br><br><br><br><br><br>
             <div style="text-align:center">
-              <h1 class="time">{{statement}}</h1>
+              <h1>{{statement}}</h1>
               {{now}}
             </div>
           `
 })
 export class AppComponent {
 
-  now = moment().format('LT');
-  time: any = this.now.split(":");
-  hour: any = parseInt(this.time[0]);
-  minute: any = parseInt(this.time[1].slice(0,3));
+  now: any;
+  time: any;
+  hour: any;
+  minute: any;
   statement: string;
-  period: string = this.time[1].slice(3).toLowerCase();
+  period: string;
+  hourWords: any;
+  minuteWords: any;
 
   words: any = {
    1 : 'one',  2 : 'two',  3 : 'three',  4 : 'four',  5 : 'five',  6 : 'six',  7 : 'seven', 8 : 'eight',  9 : 'nine',  10 : 'ten',
    11 : 'eleven',  12 : 'twelve',  13 : 'thirteen',  14 : 'fourteen', 15 : 'quarter',  16 : 'sixteen',  17 : 'seventeen',  18 : 'eighteen',  19 : 'nineteen',  20 : 'twenty',
    21 : 'twenty-one',  22 : 'twenty-two',  23 : 'twenty-three',  24 : 'twenty-four',  25 : 'twenty-five',  26 : 'twenty-six',  27 : 'twenty-seven',  28 : 'twenty-eight',  29 : 'twenty-nine',  30 : 'half',
-   31 : 'thirty-one',  32 : 'thirty-two',  33 : 'thirty-three',  34 : 'thirty-four',  35 : 'thirty-five',  36 : 'thirty-six',  37 : 'thirty-seven',  38 : 'thirty-eight',  39 : 'thirty-nine',  40 : 'forty',
-   41 : 'forty-one',  42 : 'forty-two',  43 : 'forty-three',  44 : 'forty-four',  45 : 'quarter',  46 : 'forty-six',  47 : 'forty-seven',  48 : 'forty-eight',  49 : 'forty-nine',  50 : 'fifty',
-   51 : 'fifty-one',  52 : 'fifty-two',  53 : 'fifty-three',  54 : 'fifty-four',  55 : 'fifty-five',  56 : 'fifty-six',  57 : 'fifty-seven',  58 : 'fifty-eight',  59 : 'fifty-nine'
+   45 : 'quarter'
   };
 
-  hourWords = this.words[this.hour];
-  minuteWords = this.words[this.minute];
-
   constructor (){
-      //this.toWords();
-      //setInterval(this.toWords(), 1000);
       Observable.interval(1000).subscribe(() => {
           this.toWords();
       });
     }
 
   toWords(){
+    this.now  = moment().format('LT')
+    this.time  = this.now.split(":")
+    this.hour = parseInt(this.time[0]);
+    this.minute = parseInt(this.time[1].slice(0,3));
+    this.period = this.time[1].slice(3).toLowerCase();
+    this.hourWords = this.words[this.hour];
+    this.minuteWords = this.words[this.minute];
 
     if (this.minute == 0) {
       this.statement = `It's ${this.hourWords} o'clock`;
-    } else if (this.minute <= 30) {
+    } else if (this.minute == 15) {
       this.statement = `It's ${this.minuteWords} past ${this.hourWords}`;
+    }else if (this.minute <= 30) {
+      this.statement = `It's ${this.minuteWords} minutes past ${this.hourWords}`;
     } else if (this.minute == 45) {
      this.statement = `It's ${this.minuteWords} to ${this.words[this.hour+1] }`;
+   }else if (this.minute > 30 && this.minute) {
+     this.statement = `It's ${this.words[60-this.minute]} minutes to ${this.words[this.hour+1] }`;
     } else {
       this.statement = `It's ${this.hourWords} ${this.minuteWords} ${this.period}`;
     }
